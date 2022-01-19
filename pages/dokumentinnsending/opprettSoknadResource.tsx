c247ec1921384f3cd16fd746ef6dde9ae7e59777
Øivind Kulsrud <oivind.thorsen.kulsrud@nav.no>
2022-01-19 14:04:09 +0100

    Fjerner språk fra input form

--- pages/dokumentinnsending/opprettSoknadResource.tsx
+++ pages/dokumentinnsending/opprettSoknadResource.tsx
@@ -29,7 +29,7 @@ type FormValues = {
 //https://tjenester-q1.nav.no/dokumentinnsending/opprettSoknadResource?skjemanummer=NAV%2054-00.04&erEttersendelse=false&vedleggsIder=W2,W1
 //http://localhost:3000/dokumentinnsending/opprettSoknadResource?skjemanummer=NAV%2054-00.04&erEttersendelse=false&vedleggsIder=W2,W1,
 
-// http://localhost:3000/dokumentinnsending/opprettSoknadResource?skjemanummer=NAV%2054-00.04&erEttersendelse=false&vedleggsIder=C1,W1,
+// http://localhost:3000/dokumentinnsending/opprettSoknadResource?skjemanummer=NAV%2054-00.04&sprak=NO_NB&erEttersendelse=false&vedleggsIder=C1,W1,
 // todo make the url case insensitive?
 // todo get qparams    https://reactgo.com/next-get-query-params/   <p>{query.name}</p>
 
@@ -48,7 +48,7 @@ interface Props {
  */
 const OpprettSoknadResource: NextPage = () => {
     const { query } = useRouter();
-
+    const [dokumenter, setDokumenter] = useState(null);
     const [filesUploaded, setFilesUploaded] = useState<File[]>([]);
 
     const { register, handleSubmit } = useForm<FormValues>();
@@ -58,15 +58,17 @@ const OpprettSoknadResource: NextPage = () => {
         //     data.file,
         // ]);
         console.log(data);
-        const { brukerid, sprak } = data;
+
+        const { brukerid } = data;
         const { vedleggsIder } = query;
 
         axios
             .post('http://localhost:9064/frontend/soknad', {
-                brukerid,
-                skjemanummer: query.skjemanummer,
-                sprak,
-                vedleggsListe: (vedleggsIder as string)?.split(
+                brukerId: brukerid,
+                skjemanr: query.skjemanummer,
+                sprak: query.sprak, // set bokmål som default
+                // TODO rett til vedleggsListe
+                vedleggListe: (vedleggsIder as string)?.split(
                     ',',
                 )})
             .then(() => {});
@@ -90,12 +92,7 @@ const OpprettSoknadResource: NextPage = () => {
                         defaultValue={'12345678901'}
                         {...register('brukerid')}
                     />
-                    <input
-                        type="text"
-                        placeholder="sprak"
-                        defaultValue={'NO_NB'}
-                        {...register('sprak')}
-                    />
+
                     <input type="submit" />
                     <div> {JSON.stringify(query)} </div>
                     <p>{query.skjemanummer}</p>
@@ -114,6 +111,73 @@ const OpprettSoknadResource: NextPage = () => {
       sprak=NO_NB
       vedleggListe=W1,W2 (samme som vedleggsliste)
       */}
+
+
+                    { /*
+                    vi tar inn dette
+                    vi har en skjema/søknad komponent
+                    denne har en dokument komponent, dette er
+                    {
+
+  {
+  "id": 7,
+  "innsendingsId": "7835e28d-b9cf-403f-b68d-db625fefa017",
+  "ettersendingsId": null,
+  "brukerId": "12345678901",
+  "skjemanr": "NAV 54-00.04",
+  "tittel": "Svar på forhåndsvarsel i sak om barnebidrag (bidragsmottaker)",
+  "tema": "BID",
+  "spraak": "NO_NB",
+  "status": "Opprettet",
+  "opprettetDato": "2022-01-19T13:35:51.064305",
+  "endretDato": "2022-01-19T13:35:51.064339",
+  "innsendtDato": null,
+  "vedleggsListe": [
+    {
+      "id": 11,
+      "vedleggsnr": "NAV 54-00.04",
+      "tittel": "Svar på forhåndsvarsel i sak om barnebidrag (bidragsmottaker)",
+      "uuid": "85941dba-27ee-4b18-a263-51e24f89d57b",
+      "mimetype": null,
+      "document": null,
+      "erHoveddokument": true,
+      "erVariant": false,
+      "erPdfa": true,
+      "skjemaurl": "https://cdn.sanity.io/files/gx9wf39f/soknadsveiviser-p/b896b856728119c489376644df4a189b339ffe62.pdf",
+      "opplastingsStatus": "IKKE_VALGT",
+      "opprettetdato": "2022-01-19T13:35:51.082981"
+    },
+    {
+      "id": 12,
+      "vedleggsnr": "NAV 10-07.75",
+      "tittel": "Arbeidslogg for utprøving av Innowalk som grunnlag for helhetsvurdering og vedlegg til søknad ",
+      "uuid": "85190897-e17b-43d6-a8ac-375e87d7af8e",
+      "mimetype": null,
+      "document": null,
+      "erHoveddokument": false,
+      "erVariant": false,
+      "erPdfa": false,
+      "skjemaurl": "",
+      "opplastingsStatus": "IKKE_VALGT",
+      "opprettetdato": "2022-01-19T13:35:51.087687"
+    },
+    {
+      "id": 13,
+      "vedleggsnr": "W1",
+      "tittel": "Dokumentasjon på mottatt bidrag",
+      "uuid": "6a2b67d2-b6aa-45bd-874b-7efaf9876f66",
+      "mimetype": null,
+      "document": null,
+      "erHoveddokument": false,
+      "erVariant": false,
+      "erPdfa": false,
+      "skjemaurl": null,
+      "opplastingsStatus": "IKKE_VALGT",
+      "opprettetdato": "2022-01-19T13:35:51.091965"
+    }
+  ]
+}
+                    */}
                 </form>
             </main>
 
