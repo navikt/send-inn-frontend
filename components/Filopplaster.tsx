581268f808f00221af22596ef360526979316900
Thomas Tykesson <thomas@tykesson.com>
2022-03-16 08:48:24 +0100

    Flyttet filopplaster inn i egen komponent + Oppdatering av tekster/styling

---
+++ components/Filopplaster.tsx
@@ -0,0 +1,141 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+import { useForm, SubmitHandler, useWatch } from 'react-hook-form';
+import { useState } from 'react';
+import axios from 'axios';
+import {
+    Button,
+    Label,
+    BodyShort,
+    TextField,
+} from '@navikt/ds-react';
+import { Download } from '@navikt/ds-icons';
+import { setOpplastingStatusType } from './Vedlegg';
+
+type FormValues = {
+    filnavn: string | null;
+    file: FileList | null;
+};
+
+interface FilopplasterProps {
+    innsendingsId: string;
+    id: number;
+    setOpplastingStatus: setOpplastingStatusType;
+}
+
+type OpplastetFil = {
+    id: string;
+    filnavn: string;
+};
+
+export function Filopplaster(props: FilopplasterProps) {
+    const { innsendingsId, id, setOpplastingStatus } = props;
+
+    const [filListe, setFilListe] = useState<OpplastetFil[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const { register, handleSubmit, reset, setValue, control } =
+        useForm<FormValues>();
+
+    const fileRef = useRef<HTMLInputElement | null>(null);
+    const { ref, ...rest } = register('file');
+    const watchFile = useWatch({
+        control,
+        name: 'file',
+    });
+
+    const onSubmit: SubmitHandler<FormValues> = useCallback(
+        (data) => {
+            if (!data.file?.length) {
+                console.log('Fil ikke valgt!');
+            } else {
+                if (!data.filnavn) {
+                    data.filnavn = 'Opplastetfil';
+                }
+                const fil = data.file[0];
+                console.log(data);
+
+                const formData = new FormData();
+                formData.append('file', fil);
+
+                setIsLoading(true);
+                axios
+                    .post(
+                        `http://localhost:9064/frontend/v1/soknad/${innsendingsId}/vedlegg/${id}/fil`,
+                        formData,
+                        {
+                            headers: {
+                                'Content-Type': 'multipart/form-data',
+                            },
+                        },
+                    )
+                    .then((response) => {
+                        //setSoknad(response.data);
+                        setFilListe([
+                            ...filListe,
+                            {
+                                id: response.data.id,
+                                filnavn: fil.name,
+                            },
+                        ]);
+                        setOpplastingStatus(id, 'LASTET_OPP');
+                        console.log({ response: response.data });
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    })
+                    .finally(() => {
+                        reset({ filnavn: null });
+                        setValue('file', null);
+                        setIsLoading(false);
+                        if (fileRef.current) {
+                            fileRef.current.value = '';
+                        }
+                    });
+            }
+        },
+        [
+            filListe,
+            id,
+            innsendingsId,
+            reset,
+            setOpplastingStatus,
+            setValue,
+        ],
+    );
+
+    useEffect(() => {
+        console.log('Fil endret', watchFile);
+        if (watchFile) {
+            console.log('Starter filopplasting...');
+            handleSubmit(onSubmit)();
+        }
+    }, [watchFile, handleSubmit, onSubmit]);
+    return (
+        <form>
+            <Button
+                as="label"
+                variant="secondary"
+                loading={isLoading}
+            >
+                <Download />
+                Velg dine filer
+                <input
+                    {...rest}
+                    multiple
+                    type="file"
+                    // TODO?: Støtte for drag&drop. Kan ikke bruke display: none. Eksempel på løsning: https://stackoverflow.com/a/44277812/15886307
+                    style={{ display: 'none' }}
+                    ref={(e) => {
+                        ref(e);
+                        fileRef.current = e; // you can still assign to ref
+                    }}
+                />
+            </Button>
+            <TextField
+                label="Gi en beskrivende tittel på vedlegget"
+                description="Hvis dette er en av flere sider i et dokument, oppgi sidetall av totalt antall sider (eks. Epikrise, side 1 av 5)."
+                {...register('filnavn')}
+            />
+        </form>
+    );
+}
--- components/Vedlegg.tsx
+++ components/Vedlegg.tsx
@@ -1,23 +1,10 @@
-import React, {
-    FC,
-    ReactElement,
-    useCallback,
-    useEffect,
-    useRef,
-} from 'react';
-import Link from 'next/link';
-import { useForm, SubmitHandler, useWatch } from 'react-hook-form';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import axios from 'axios';
-import { Button, Panel } from '@navikt/ds-react';
-import { Download } from '@navikt/ds-icons';
+import { Panel } from '@navikt/ds-react';
+import { Filopplaster } from './Filopplaster';
 
-type FormValues = {
-    filnavn: string | null;
-    file: FileList | null;
-};
-
-type setOpplastingStatusType = (
+export type setOpplastingStatusType = (
     id: number,
     opplastingStatus: string,
 ) => void;
@@ -91,10 +78,6 @@ function Vedlegg(props: VedleggProps) {
     } = props;
 
     const [filListe, setFilListe] = useState<OpplastetFil[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
-
-    const { register, handleSubmit, reset, setValue, control } =
-        useForm<FormValues>();
 
     useEffect(() => {
         //const innsendingsId = query.innsendingsId // todo fix, fungerer ikke med en gang om man henter herifra, må kan
@@ -127,80 +110,6 @@ function Vedlegg(props: VedleggProps) {
         // }, [innsendingsId, id, filListe]); // loop
     }, [innsendingsId, id]);
 
-    const fileRef = useRef<HTMLInputElement | null>(null);
-    const { ref, ...rest } = register('file');
-    const watchFile = useWatch({
-        control,
-        name: 'file',
-    });
-
-    const onSubmit: SubmitHandler<FormValues> = useCallback(
-        (data) => {
-            if (!data.file?.length) {
-                console.log('Fil ikke valgt!');
-            } else {
-                if (!data.filnavn) {
-                    data.filnavn = 'Opplastetfil';
-                }
-                const fil = data.file[0];
-                console.log(data);
-
-                const formData = new FormData();
-                formData.append('file', fil);
-
-                setIsLoading(true);
-                axios
-                    .post(
-                        `http://localhost:9064/frontend/v1/soknad/${innsendingsId}/vedlegg/${id}/fil`,
-                        formData,
-                        {
-                            headers: {
-                                'Content-Type': 'multipart/form-data',
-                            },
-                        },
-                    )
-                    .then((response) => {
-                        //setSoknad(response.data);
-                        setFilListe([
-                            ...filListe,
-                            {
-                                id: response.data.id,
-                                filnavn: fil.name,
-                            },
-                        ]);
-                        setOpplastingStatus(id, 'LASTET_OPP');
-                        console.log({ response: response.data });
-                    })
-                    .catch((error) => {
-                        console.error(error);
-                    })
-                    .finally(() => {
-                        reset({ filnavn: null });
-                        setValue('file', null);
-                        setIsLoading(false);
-                        if (fileRef.current) {
-                            fileRef.current.value = '';
-                        }
-                    });
-            }
-        },
-        [
-            filListe,
-            id,
-            innsendingsId,
-            reset,
-            setOpplastingStatus,
-            setValue,
-        ],
-    );
-
-    useEffect(() => {
-        console.log('Fil endret', watchFile);
-        if (watchFile) {
-            console.log('Starter filopplasting...');
-            handleSubmit(onSubmit)();
-        }
-    }, [watchFile, handleSubmit, onSubmit]);
     return (
         <Panel border>
             <div>
@@ -218,31 +127,11 @@ function Vedlegg(props: VedleggProps) {
             <div>
                 {vedleggsnr}: {label}
             </div>
-            <form>
-                <br />
-                Beskriv vedlegg:
-                <input {...register('filnavn')} />
-                <br />
-                <Button
-                    as="label"
-                    variant="secondary"
-                    loading={isLoading}
-                >
-                    <Download />
-                    Velg dine filer
-                    <input
-                        {...rest}
-                        multiple
-                        type="file"
-                        // TODO?: Støtte for drag&drop. Kan ikke bruke display: none. Eksempel på løsning: https://stackoverflow.com/a/44277812/15886307
-                        style={{ display: 'none' }}
-                        ref={(e) => {
-                            ref(e);
-                            fileRef.current = e; // you can still assign to ref
-                        }}
-                    />
-                </Button>
-            </form>
+            <Filopplaster
+                id={id}
+                innsendingsId={innsendingsId}
+                setOpplastingStatus={setOpplastingStatus}
+            />
             {filListe.length > 0 && (
                 <div>
                     <span>Dokumenter du har lastet opp nå:</span>
