d38381eb215b0b6fb71430b6e54d32d6831f6365
Øivind Kulsrud <oivind.thorsen.kulsrud@nav.no>
2022-03-18 13:13:03 +0100

    styledjsx virker

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,23 +1,22 @@
-import styled from 'styled-components';
-
-const Content = styled.div`
-    font-size: 1.5em;
-    text-align: center;
-    font-family: arial;
-    line-height: 1.5;
-    max-width: 900px;
-`;
-
 // TODO fix: Binding element 'children' implicitly has an 'any' type.ts(7031)
 const Layout = ({ children }) => {
     return (
         <div className="content">
             <h1> header</h1>
-            <Content>
+            <div>
                 {children}
                 {/* footer could go here */}
-            </Content>
+            </div>
             <h1> footer</h1>
+            <style jsx>{`
+                .content {
+                    font-size: 1.5em;
+                    text-align: center;
+                    font-family: arial;
+                    line-height: 1.5;
+                    max-width: 900px;
+                }
+            `}</style>
         </div>
     );
 };
