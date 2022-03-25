bd0873053ef103f7c439857a42e0f369410e884b
Thomas Tykesson <thomas@tykesson.com>
2022-03-25 12:28:13 +0100

    Lagt inn støtte for SR med styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import Layout from '../components/Layout';
+import Layout from '../components/LayoutSC';
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <Layout>
