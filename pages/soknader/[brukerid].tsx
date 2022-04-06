3f3cc961ee2fd5e72c344d4b5389277764f68a06
Øivind Kulsrud <oivind.thorsen.kulsrud@nav.no>
2022-04-06 13:35:47 +0200

    Implementerer støtte for flere språk
    virker

--- pages/soknader/[brukerid].tsx
+++ pages/soknader/[brukerid].tsx
@@ -7,7 +7,6 @@ import { useRouter, NextRouter } from 'next/router';
 import { GetServerSideProps } from 'next';
 import axios from 'axios';
 import Vedlegg from '../../components/Vedlegg';
-import TestCompState from '../../components/TestCompState';
 
 import { VedleggType, SoknadType } from '../../types/types';
 import Link from 'next/link';
