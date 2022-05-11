e532bf1e5102feedf055264bff6384d2de09020e
Thomas Tykesson <thomas@tykesson.com>
2022-05-11 14:36:59 +0200

    Testing av tokenx exchange

---
+++ pages/api/tokenx.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getTokenxToken } from '../../auth/getTokenXToken';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
+    const idportenToken = req.headers.authorization!.split(' ')[1];
+    const tokenxToken = await getTokenxToken(
+        idportenToken,
+        process.env.INNSENDING_API_AUDIENCE,
+    );
+    res.status(200).json({ tokenxToken, idportenToken });
+}
