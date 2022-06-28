1746fc67fe5ee64388e9528142561b8a4a20a872
Thomas Tykesson <thomas@tykesson.com>
2022-06-28 13:47:37 +0200

    Flytter index til /dev/index

--- pages/dev/index.tsx
+++ pages/dev/index.tsx
@@ -1,9 +1,5 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-// import styles from '../styles/Home.module.css';
-import Link from 'next/link';
-
-import '../i18n';
 
 const Home: NextPage = () => {
     return (
