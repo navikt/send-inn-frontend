1022c002a23b51ef07c3903877a8f988549aab67
Øivind <oivind.thorsen.kulsrud@nav.no>
2022-10-17 18:59:39 +0200

    endringer i 500 og 404 tekst og design

--- pages/500.js
+++ pages/500.js
@@ -19,45 +19,74 @@ export default function Custom500() {
                 <Heading level="1" size="xlarge" spacing>
                     Det har skjedd en feil
                 </Heading>
-
-                <Detail spacing>Statuskode 500</Detail>
             </div>
             <div>
                 <div>
                     <div>
                         <BodyShort spacing>
                             Beklager, vi har tekniske problemer. Prøv
-                            igjen om en time. Om problemet vedvarer ta
-                            gjerne kontakt med NAV på lenken nedenfor.
+                            igjen senere. Om problemet vedvarer ta
+                            kontakt med NAV for å få hjelp på telefon
+                            (+47) 55 55 33 33 eller på ditt lokale
+                            NAV-kontor.
+                        </BodyShort>
+
+                        <BodyShort spacing>
+                            Vi setter pris på om du melder fra om
+                            feilen via lenken nedenfor.
                         </BodyShort>
 
                         <BodyShort spacing>
                             <a href="https://www.nav.no/person/kontakt-oss/tilbakemeldinger/feil-og-mangler">
-                                Meld gjerne fra om denne feilen
+                                Meld fra om denne feilen
+                            </a>
+                        </BodyShort>
+
+                        <BodyShort spacing>
+                            <a href="https://nav.no">
+                                Gå til forsiden på nav.no
                             </a>
                         </BodyShort>
                     </div>
 
                     <div>
-                        <Heading level="2" size="large" spacing>
-                            In English
+                        <Heading level="1" size="xlarge" spacing>
+                            An error has occured
                         </Heading>
+                    </div>
+                    <div>
+                        <div>
+                            <BodyShort spacing>
+                                We are experiencing technical
+                                difficulties. We apologize for the
+                                inconvenience. Please try again later.
+                                If the problem persists, you can get
+                                help from NAV Service Centre at (+47)
+                                55 55 33 33 or at your local NAV
+                                office.
+                            </BodyShort>
 
-                        <BodyShort spacing>
-                            We are experiencing technical
-                            difficulties. We apologize for any
-                            inconvenience caused. If the problem
-                            persists please contact NAV at the adress
-                            below.
-                        </BodyShort>
-                        <BodyShort spacing>
-                            Go to the{' '}
-                            <a href="https://nav.no">front page</a>,
-                            or use one of the links in the menu.
-                        </BodyShort>
+                            <BodyShort spacing>
+                                We would appreciate it if you report
+                                the error via the link below.
+                            </BodyShort>
+
+                            <BodyShort spacing>
+                                <a href="https://www.nav.no/person/kontakt-oss/tilbakemeldinger/feil-og-mangler">
+                                    Report this error
+                                </a>
+                            </BodyShort>
+
+                            <BodyShort spacing>
+                                <a href="https://nav.no">
+                                    Return to nav.no front page
+                                </a>
+                            </BodyShort>
+                        </div>
                     </div>
                 </div>
             </div>
+            <Detail spacing>Statuskode 500</Detail>
         </Style>
     );
 }
