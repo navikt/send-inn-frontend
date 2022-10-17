4d0bee2ae2c70abf0136925121b2e11cac79ac96
Thomas Tykesson <thomas@tykesson.com>
2022-10-17 08:32:45 +0200

    [innsendingsId].tsx: Fjerner ubrukt kode, og rydder i kommentarer

--- pages/ettersending/[innsendingsId].tsx
+++ pages/ettersending/[innsendingsId].tsx
@@ -1,59 +1,18 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { useState, createContext, useEffect } from 'react';
-import { useRouter, NextRouter } from 'next/router';
-import { GetServerSideProps } from 'next';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import axios, { AxiosError } from 'axios';
-import Vedlegg from '../../components/Vedlegg';
 import VedleggsListe from '../../components/VedleggsListe';
 import { SoknadHeader } from '../../components/SoknadHeader';
 
 import { VedleggType, SoknadType } from '../../types/types';
-// todo https://dev.to/fadiamg/multiple-file-inputs-with-one-submit-button-with-react-hooks-kle
 
 import getConfig from 'next/config';
 
 const { publicRuntimeConfig } = getConfig();
 const erEttersending = true;
-type FormValues = {
-    file: File;
-    brukerid: string;
-    sprak: string;
-};
-
-/*
-interface VedleggProps {
-    innsendingsId: string;
-    id: number;
-    vedleggsnr: string;
-    tittel: string;
-    uuid: string;
-    mimetype: string | null;
-    document: string | null;
-    erHoveddokument: boolean;
-    erVariant: boolean;
-    erPdfa: boolean;
-    skjemaurl: string | null;
-    opplastingsStatus: string;
-    opprettetdato: string;
-}
-*/
-
-type VedleggProps = VedleggType & {
-    innsendingsId: string | undefined;
-};
-
-interface contextValue {
-    value: VedleggProps | null;
-}
-
-// same const ThemeContext = React.createContext(themes.light);
-// why is this duplicated?
-export const AppContext = React.createContext<
-    VedleggProps | undefined
->(undefined);
 
 const initialVedleggsliste: VedleggType[] | [] = [];
 
@@ -64,23 +23,12 @@ const EttersendingSide: NextPage = () => {
     const [vedleggsListe, setVedleggsListe] = useState<VedleggType[]>(
         initialVedleggsliste,
     );
-    const { register, handleSubmit } = useForm<FormValues>();
     const innsendingsId = query.innsendingsId;
     useEffect(() => {
-        //const innsendingsId = query.innsendingsId // todo fix, fungerer ikke med en gang om man henter herifra, må kan
-        // const innsendingsId = "d83c88e4-a3f3-4217-b561-fe0572e391e8";
-        //const { brukerid } = data;
-        //const { vedleggsIder } = query;
         if (innsendingsId) {
             axios
                 .get(
-                    `${publicRuntimeConfig.apiUrl}/frontend/v1/soknad/${innsendingsId}` /*, {
-                brukerId: brukerid,
-                skjemanr: query.skjemanummer,
-                sprak: query.sprak, // set bokmål som default
-                // TODO rett til vedleggsListe
-                vedleggsListe: (vedleggsIder as string)?.split(','),
-            }*/,
+                    `${publicRuntimeConfig.apiUrl}/frontend/v1/soknad/${innsendingsId}`,
                 )
                 .then((response) => {
                     setSoknad(response.data);
@@ -100,28 +48,6 @@ const EttersendingSide: NextPage = () => {
         }
     }, [innsendingsId, router]);
 
-    const onSubmit: SubmitHandler<FormValues> = (data) => {
-        console.log(data);
-        const innsendingsId = query.innsendingsId;
-        //const { brukerid } = data;
-        //const { vedleggsIder } = query;
-
-        axios
-            .get(
-                `${publicRuntimeConfig.apiUrl}/frontend/v1/soknad/${innsendingsId}` /*, {
-                brukerId: brukerid,
-                skjemanr: query.skjemanummer,
-                sprak: query.sprak, // set bokmål som default
-                // TODO rett til vedleggsListe
-                vedleggsListe: (vedleggsIder as string)?.split(','),
-            }*/,
-            )
-            .then((response) => {
-                setSoknad(response.data);
-                setVedleggsListe(response.data.vedleggsListe);
-                console.log({ response: response.data });
-            });
-    };
     return (
         <div>
             <Head>
@@ -153,18 +79,8 @@ const EttersendingSide: NextPage = () => {
                     </>
                 )}
             </main>
-            <footer></footer>
         </div>
     );
 };
-
-/*
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // get whatever you can get from the service at rendertime here  
-  // return {props: {hello: 'hello'}}
-  prop : Props = {somevalue = 'string'}
-  return prop
-}
-*/
 
 export default EttersendingSide;
