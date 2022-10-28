f2f988d6e8c6e28364c33ed9e54623ff80e904f9
oivindkulsrud <oivind.thorsen.kulsrud@nav.no>
2022-10-28 12:50:55 +0200

    404/500 design (#105)
    * Oppdaterer env.local.example med ny env variabel for minside url
    
    * Forbedret design på feilsidene

--- pages/500.js
+++ pages/500.js
@@ -1,4 +1,11 @@
-import { BodyShort, Search, Detail, Heading } from '@navikt/ds-react';
+import styled from 'styled-components';
+import {
+    BodyShort,
+    Detail,
+    Heading,
+    GuidePanel,
+} from '@navikt/ds-react';
+
 const Style = styled.div`
     min-height: 100vh;
     max-width: 50rem;
@@ -7,86 +14,93 @@ const Style = styled.div`
     margin-bottom: 44px;
 `;
 
-const DivWithSpacing = styled.div`
-    margin-bottom: 16px;
+const HorizontalLinksFlexbox = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
 `;
-import styled from 'styled-components';
 
 export default function Custom500() {
     return (
         <Style>
-            <div>
-                <Heading level="1" size="xlarge" spacing>
-                    Det har skjedd en feil
-                </Heading>
-            </div>
-            <div>
+            <GuidePanel poster="true">
                 <div>
-                    <div>
-                        <BodyShort spacing>
-                            Beklager, vi har tekniske problemer. Prøv
-                            igjen senere. Om problemet vedvarer ta
-                            kontakt med NAV for å få hjelp på telefon
-                            (+47) 55 55 33 33 eller på ditt lokale
-                            NAV-kontor.
-                        </BodyShort>
-
-                        <BodyShort spacing>
-                            Vi setter pris på om du melder fra om
-                            feilen via lenken nedenfor.
-                        </BodyShort>
-
-                        <BodyShort spacing>
-                            <a href="https://www.nav.no/person/kontakt-oss/tilbakemeldinger/feil-og-mangler">
-                                Meld fra om denne feilen
-                            </a>
-                        </BodyShort>
-
-                        <BodyShort spacing>
-                            <a href="https://nav.no">
-                                Gå til forsiden på nav.no
-                            </a>
-                        </BodyShort>
-                    </div>
-
-                    <div>
-                        <Heading level="1" size="xlarge" spacing>
-                            An error has occured
-                        </Heading>
-                    </div>
+                    <Heading level="1" size="xlarge" spacing>
+                        Det har skjedd en feil
+                    </Heading>
+                </div>
+                <div>
                     <div>
                         <div>
                             <BodyShort spacing>
-                                We are experiencing technical
-                                difficulties. We apologize for the
-                                inconvenience. Please try again later.
-                                If the problem persists, you can get
-                                help from NAV Service Centre at (+47)
-                                55 55 33 33 or at your local NAV
-                                office.
+                                Beklager, vi har tekniske problemer.
+                                Prøv igjen senere. Om problemet
+                                vedvarer ta kontakt med NAV for å få
+                                hjelp på telefon (+47) 55 55 33 33
+                                eller på ditt lokale NAV-kontor.
                             </BodyShort>
 
                             <BodyShort spacing>
-                                We would appreciate it if you report
-                                the error via the link below.
+                                Vi setter pris på om du melder fra om
+                                feilen via lenken nedenfor.
                             </BodyShort>
+                            <HorizontalLinksFlexbox>
+                                <BodyShort spacing>
+                                    <a href="https://www.nav.no/person/kontakt-oss/tilbakemeldinger/feil-og-mangler">
+                                        Meld fra om denne feilen
+                                    </a>
+                                </BodyShort>
 
-                            <BodyShort spacing>
-                                <a href="https://www.nav.no/person/kontakt-oss/tilbakemeldinger/feil-og-mangler">
-                                    Report this error
-                                </a>
-                            </BodyShort>
+                                <BodyShort spacing>
+                                    <a href="https://nav.no">
+                                        Gå til forsiden på nav.no
+                                    </a>
+                                </BodyShort>
+                            </HorizontalLinksFlexbox>
+                        </div>
 
-                            <BodyShort spacing>
-                                <a href="https://nav.no">
-                                    Return to nav.no front page
-                                </a>
-                            </BodyShort>
+                        <div>
+                            <Heading level="1" size="xlarge" spacing>
+                                An error has occured
+                            </Heading>
+                        </div>
+                        <div>
+                            <div>
+                                <BodyShort spacing>
+                                    We are experiencing technical
+                                    difficulties. We apologize for the
+                                    inconvenience. Please try again
+                                    later. If the problem persists,
+                                    you can get help from NAV Service
+                                    Centre at (+47) 55 55 33 33 or at
+                                    your local NAV office.
+                                </BodyShort>
+
+                                <BodyShort spacing>
+                                    We would appreciate it if you
+                                    report the error via the link
+                                    below.
+                                </BodyShort>
+                                <HorizontalLinksFlexbox>
+                                    <BodyShort spacing>
+                                        <a href="https://www.nav.no/person/kontakt-oss/tilbakemeldinger/feil-og-mangler">
+                                            Report this error
+                                        </a>
+                                    </BodyShort>
+
+                                    <BodyShort spacing>
+                                        <a href="https://nav.no">
+                                            Return to nav.no front
+                                            page
+                                        </a>
+                                    </BodyShort>
+                                </HorizontalLinksFlexbox>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <Detail spacing>Statuskode 500</Detail>
+                <Detail spacing>Statuskode 500</Detail>
+            </GuidePanel>
         </Style>
     );
 }
