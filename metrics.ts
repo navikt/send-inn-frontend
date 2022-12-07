79c45ae5b5f1346c7e1a7cb7b4e0733cf90ba84a
Thomas Tykesson <thomas@tykesson.com>
2022-12-07 10:01:51 +0100

    Legger til prom-client, og prometheus-endepunkt med default metrics

---
+++ metrics.ts
@@ -0,0 +1,18 @@
+import { collectDefaultMetrics } from 'prom-client';
+
+declare global {
+    // eslint-disable-next-line no-var
+    var _metrics: AppMetrics;
+}
+
+export class AppMetrics {
+    constructor() {
+        console.info('Initializing metrics client');
+
+        collectDefaultMetrics();
+    }
+}
+
+global._metrics = global._metrics || new AppMetrics();
+
+export default global._metrics;
