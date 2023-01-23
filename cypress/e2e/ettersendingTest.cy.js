6a1140e0d1359629e35b6fe8bc0e7343a7536efe
Thomas Tykesson <thomas@tykesson.com>
2023-01-23 09:08:52 +0100

    Optimaliserer render av filvelgerLabel, som skapte ustabilit i Cypress. Optimalisert tester

--- cypress/e2e/ettersendingTest.cy.js
+++ cypress/e2e/ettersendingTest.cy.js
@@ -10,43 +10,45 @@ describe('Tester ettersendingsløpet', () => {
         cy.get('[data-cy="filvelgerKnapp"]', {
             timeout: 10000,
         }).should('be.visible');
-        cy.wait(500);
+
+        cy.get('[data-cy="filvelgerKnapp"]').should('have.length', 3);
 
         cy.get('[data-cy="filvelgerKnapp"]', {
             timeout: 10000,
         })
             .eq(0)
+            .should('be.visible')
             .click();
 
         cy.get('[data-cy="filvelgerKnapp"]')
             .eq(0)
+            .should('be.visible')
             .selectFile('cypress/fixtures/MarcusAurelius.jpeg');
-
-        cy.wait(20);
 
         cy.get('[data-cy="fileUploadSuccessIkon"]', {
             timeout: 10000,
         }).should('be.visible');
 
-        cy.wait(500);
-        cy.get('[data-cy="sendSenereRadio"]').eq(0).click();
-        cy.wait(1500);
-        cy.get('[data-cy="sendSenereRadio"]').eq(1).click();
+        cy.get('[data-cy="sendSenereRadio"]')
+            .eq(0)
+            .click()
+            .should('be.checked');
 
-        cy.wait(1500);
+        cy.get('[data-cy="sendSenereRadio"]')
+            .eq(1)
+            .click()
+            .should('be.checked');
+
         cy.get('[data-cy="sendTilNAVKnapp"]', {
             timeout: 10000,
         }).click();
-        cy.wait(1000);
 
         cy.get('[data-cy="jaFellesModalKnapp"]', {
             timeout: 10000,
         }).should('be.visible');
-        cy.wait(1000);
 
         cy.get('[data-cy="jaFellesModalKnapp"]').click();
 
-        cy.wait(20);
         cy.get('[data-cy="kvitteringOverskrift"]', {
             timeout: 10000,
         }).should('be.visible');
