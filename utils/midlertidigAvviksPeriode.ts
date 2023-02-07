e4cf3d1badc600a3ca5946ea2ff5791aa53734c8
Thomas Tykesson <thomas@tykesson.com>
2023-02-07 15:34:53 +0100

    Legger til ny aviksmelding, som erstatter gammel (utgått)

--- utils/midlertidigAvviksPeriode.ts
+++ utils/midlertidigAvviksPeriode.ts
@@ -1,8 +1,8 @@
-//TODO: denne filen kan fjernes i februar 2023
+//TODO: denne filen kan fjernes i etter 4. april
 /* Brukes til å generere melding om et spesifikt avvik */
 export const erDatoIAvviksPeriode = (date) => {
     const avvikStart = new Date('2022-12-12T16:50:00.000Z');
-    const avvikSlutt = new Date('2022-12-14T10:50:00.000Z');
+    const avvikSlutt = new Date('2023-02-07T13:00:00.000Z');
     const opprettet = new Date(date);
     return (
         avvikStart.getTime() <= opprettet.getTime() &&
