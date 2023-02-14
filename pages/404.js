66ed1bbcd8ed403585cb80aab9548802a2157b9e
Thomas Tykesson <thomas@tykesson.com>
2023-02-14 10:28:19 +0100

    Nye tekster for 404-side + flytting av tekster til oversettelsesfil

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,10 @@ import {
     Detail,
     Heading,
     GuidePanel,
+    Link as NavLink,
 } from '@navikt/ds-react';
+import { useTranslation } from 'react-i18next';
+import Head from 'next/head';
 
 const Style = styled.div`
     min-height: 100vh;
@@ -18,86 +21,66 @@ const HorizontalLinksFlexbox = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
+    margin-bottom: 3rem;
 `;
 
 export default function Custom404() {
+    const { t } = useTranslation();
+
+    const sprakListe = ['nb', 'nn', 'en'];
     return (
         <Style>
+            <Head>
+                <title>{t('feilside.404.tittel')}</title>
+            </Head>
             <GuidePanel poster="true">
-                <div>
-                    <Heading level="1" size="xlarge" spacing>
-                        Vi fant ikke den siden
-                    </Heading>
-                </div>
-                <div>
-                    <div>
-                        <div>
-                            <BodyShort spacing>
-                                Beklager, siden kan være slettet eller
-                                flyttet, eller det var en feil i
-                                lenken som førte deg hit.
-                            </BodyShort>
-                            <BodyShort spacing>
-                                Vi setter pris på om du melder fra om
-                                feilen via lenken nedenfor.
-                            </BodyShort>
-                        </div>
+                {sprakListe.map((sprak) => (
+                    <div key={sprak}>
+                        <Heading level="1" size="xlarge" spacing>
+                            {t('feilside.404.overskrift', {
+                                lng: sprak,
+                            })}
+                        </Heading>
+                        <BodyShort spacing>
+                            {t('feilside.404.p1', {
+                                lng: sprak,
+                            })}
+                        </BodyShort>
+                        <BodyShort spacing>
+                            {t('feilside.404.p2', {
+                                lng: sprak,
+                            })}
+                        </BodyShort>
                         <HorizontalLinksFlexbox>
                             <BodyShort spacing>
-                                <a href="https://nav.no">
-                                    Gå til forsiden på nav.no
-                                </a>
+                                <NavLink href="https://nav.no">
+                                    {t(
+                                        'feilside.404.lenkeTekst.forside',
+                                        {
+                                            lng: sprak,
+                                        },
+                                    )}
+                                </NavLink>
                             </BodyShort>
                             <BodyShort spacing>
-                                <a href="https://www.nav.no/person/kontakt-oss/tilbakemeldinger/feil-og-mangler">
-                                    Meld fra om denne feilen
-                                </a>
+                                <NavLink
+                                    href={`https://www.nav.no/person/kontakt-oss/${sprak}/tilbakemeldinger/feil-og-mangler`}
+                                >
+                                    {t(
+                                        'feilside.404.lenkeTekst.meldFra',
+                                        {
+                                            lng: sprak,
+                                        },
+                                    )}
+                                </NavLink>
                             </BodyShort>
                         </HorizontalLinksFlexbox>
                     </div>
+                ))}
 
-                    <div>
-                        <Heading level="1" size="xlarge" spacing>
-                            We did not find that page
-                        </Heading>
-                    </div>
-                    <div>
-                        <div>
-                            <div>
-                                <div>
-                                    <BodyShort spacing>
-                                        The page you requested cannot
-                                        be found. It may have been
-                                        deleted, moved, or there may
-                                        be an error in the link that
-                                        led you here. We appologize
-                                        for the inconvenience.
-                                    </BodyShort>
-                                    <BodyShort spacing>
-                                        We would appreciate it if you
-                                        report the error via the link
-                                        below.
-                                    </BodyShort>
-                                </div>
-                                <HorizontalLinksFlexbox>
-                                    <BodyShort spacing>
-                                        <a href="https://nav.no">
-                                            Return to nav.no front
-                                            page
-                                        </a>
-                                    </BodyShort>
-                                    <BodyShort spacing>
-                                        <a href="https://www.nav.no/person/kontakt-oss/tilbakemeldinger/feil-og-mangler">
-                                            Report this error
-                                        </a>
-                                    </BodyShort>
-                                </HorizontalLinksFlexbox>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <Detail spacing>Statuskode 404</Detail>
+                <Detail spacing>
+                    {t('feilside.statuskode')} 404
+                </Detail>
             </GuidePanel>
         </Style>
     );
