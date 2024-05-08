41ae5dcd39a7acb54157ad34d69acddd7e501634
Morten Stulen <morten.elias.stulen@nav.no>
2024-05-08 10:01:13 +0200

    Add test

--- cypress/e2e/uxSignals.cy.js
+++ cypress/e2e/uxSignals.cy.js
@@ -1,7 +1,11 @@
 describe('uxSignals', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/sendinn/api/fyllut/forms/*', cy.spy().as('getFormSpy'));
+  });
   it('Should show uxSignals', () => {
     cy.defaultIntercepts();
     cy.visit('/fyll-ut-default');
+    cy.get('@getFormSpy').should('have.been.calledOnce');
 
     cy.findAllByRole('radio', { name: 'Jeg laster opp dette senere' }).eq(0).click();
     cy.findAllByRole('radio', { name: 'Jeg laster opp dette senere' }).eq(1).click();
@@ -12,5 +16,11 @@ describe('uxSignals', () => {
 
     // The first question in the uxSignals form
     cy.findByText('Hvor gammel er du?').should('exist');
+  });
+
+  it('Should not make request to fyllut if visningsType is dokumentinnsending', () => {
+    cy.defaultIntercepts();
+    cy.visit('/dokumentinnsending-default');
+    cy.get('@getFormSpy').should('not.have.been.called');
   });
 });
