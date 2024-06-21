ed341f8c33d50531e5a743559a235d5afdf5ff5d
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2024-06-21 08:57:19 +0200

    FIX uxSignals cypress test

--- mocks/handlers/uxSignals.ts
+++
@@ -1,8 +0,0 @@
-import { HttpResponse, http } from 'msw';
-import { uxSignalsMock } from '../data/uxsignals';
-
-export const getUxSignals = http.get('https://api.uxsignals.com/v2/study/id/:uxSignalsId', async ({ params }) => {
-  const { uxSignalsId } = params;
-  const fyllutForm = await uxSignalsMock(uxSignalsId as string);
-  return HttpResponse.json(fyllutForm, { status: 200 });
-});
