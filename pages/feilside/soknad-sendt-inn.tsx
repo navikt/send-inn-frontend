fb45284c95d3e242b666fa951124ce3770593c8d
nurrebass1 <16683497+nurrebass1@users.noreply.github.com>
2025-05-19 13:01:14 +0200

    Feilmelding avsluttet sak

--- pages/feilside/soknad-sendt-inn.tsx
+++ pages/feilside/soknad-sendt-inn.tsx
@@ -1,67 +1,66 @@
-import { BodyShort, Detail, GuidePanel, Heading, Link as NavLink } from '@navikt/ds-react';
+import { BodyShort, Box, Heading, Page, VStack } from '@navikt/ds-react';
 import Head from 'next/head';
+import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
-import styled from 'styled-components';
-
-const { NEXT_PUBLIC_MIN_SIDE_URL } = process.env;
-
-const Style = styled.div`
-  padding-top: 44px;
-  margin-bottom: 44px;
-`;
-
-const HorizontalLinksFlexbox = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  gap: 0 1rem;
-  margin-bottom: 3rem;
-`;
 
 export default function SoknadSendtInn() {
   const { t } = useTranslation();
 
+  const urlToLocaleUrl = (url: string, locale: string) => (locale !== 'en' ? url : `${url}/${locale}`);
+
   const sprakListe = ['nb', 'nn', 'en'];
   return (
-    <Style>
-      <Head>
-        <title>{t('feilside.soknadSendtInn.tittel')}</title>
-      </Head>
-      <GuidePanel poster={true}>
-        {sprakListe.map((sprak) => (
-          <div key={sprak}>
-            <Heading level="1" size="medium" spacing>
-              {t('feilside.soknadSendtInn.overskrift', {
-                lng: sprak,
-              })}
-            </Heading>
-            <BodyShort spacing>
-              {t('feilside.soknadSendtInn.melding', {
-                lng: sprak,
-              })}
-            </BodyShort>
-            <HorizontalLinksFlexbox>
-              <BodyShort spacing>
-                <NavLink href={NEXT_PUBLIC_MIN_SIDE_URL}>
-                  {t('feilside.soknadSendtInn.lenkeTekst.minSide', {
-                    lng: sprak,
-                  })}
-                </NavLink>
-              </BodyShort>
-              <BodyShort spacing>
-                <NavLink href={`https://www.nav.no/soknader${sprak}ettersendelse/person`}>
-                  {t('feilside.soknadSendtInn.lenkeTekst.ettersende', {
-                    lng: sprak,
-                  })}
-                </NavLink>
-              </BodyShort>
-            </HorizontalLinksFlexbox>
-          </div>
-        ))}
-
-        <Detail spacing>{t('feilside.statuskode')} 405</Detail>
-      </GuidePanel>
-    </Style>
+    <Page>
+      <Page.Block as="main" width="xl" gutters>
+        <Box paddingBlock="12">
+          <VStack gap="16">
+            <VStack gap="12" align="start">
+              {sprakListe.map((sprak) => (
+                <div key={sprak}>
+                  <Head>
+                    <title>{t('feilside.soknadSendtInn.tittel')}</title>
+                  </Head>
+                  <div>
+                    <Heading level="1" size="medium" spacing>
+                      {t('feilside.soknadSendtInn.overskrift', {
+                        lng: sprak,
+                      })}
+                    </Heading>
+                    <BodyShort spacing>
+                      {t('feilside.soknadSendtInn.innhold', {
+                        lng: sprak,
+                      })}
+                    </BodyShort>
+                    <BodyShort spacing>
+                      {t('feilside.soknadSendtInn.dokumentArkivMelding', {
+                        lng: sprak,
+                      })}
+                      <Link href={urlToLocaleUrl('https://www.nav.no/dokumentarkiv', sprak)}>
+                        {t('feilside.soknadSendtInn.lenkeTekst.dokumentArkiv', {
+                          lng: sprak,
+                        })}
+                      </Link>
+                    </BodyShort>
+                    <BodyShort spacing>
+                      {t('feilside.soknadSendtInn.ettersendingMelding', {
+                        lng: sprak,
+                      })}
+                      <Link href={urlToLocaleUrl('https://www.nav.no/ettersende', sprak)}>
+                        {t('feilside.soknadSendtInn.lenkeTekst.ettersende', {
+                          lng: sprak,
+                        })}
+                      </Link>
+                      {t('feilside.soknadSendtInn.ettersendingMelding2', {
+                        lng: sprak,
+                      })}
+                    </BodyShort>
+                  </div>
+                </div>
+              ))}
+            </VStack>
+          </VStack>
+        </Box>
+      </Page.Block>
+    </Page>
   );
 }
